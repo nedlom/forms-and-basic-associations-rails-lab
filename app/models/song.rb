dfbc3aead960e1218b0c744ec3cd52395813dbf7
Nick <nelom3@gmail.com>
2021-09-19 20:49:17 -0700

    commit tree 74ccc7c8c7a5ab6a8c3fbe8bddb9f658e9e683b5

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,35 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def note_contents=(contents)
+    contents.each do |note|
+      self.notes << Note.new(content: note)
+    end
+
+  end
+
+  def note_contents
+    if self.notes
+      self.notes.map do |note|
+        note.content
+      end
+    else
+      nil
+    end
+  end
+
 end
